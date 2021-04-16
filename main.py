06cef14813f256d479eb77e15f1f9f59978bc4d1
Filip Berglund <ryerzs@gmail.com>
2021-04-16 09:14:33 +0200

    building row_echelon form

--- main.py
+++ main.py
@@ -40,12 +40,59 @@ def loadtxt(path):
     file = open(path, mode='r', encoding='utf-8')
     return [[int(n) for n in line.split()] for line in file]
 
+def row_echelon(m):
+    newM = []
+    if len(m)==0:
+        return []
+    elif(len(m[0])==0):
+        return []
+    l = len(m[0])
+    rows = len(m)
+    base = None
+    for j in range(rows):
+        if m[j][0] != 0:
+            base = j
+            break
+    if base == None:
+        return removeColumn(m,0)
+    a = m[base][0]
+    for j in range(rows):
+        if j != base:
+            factor = m[j][0]/a
+            for i in range(l):
+                m[j][i] -= factor*m[base][i]
+    return m
+
+def removeColumn(m, c):
+    out = []
+    l = len(m[0])
+    rows = len(m)
+    for j in range(rows):
+        row = []
+        for i in range(l):
+            if i != c:
+                row.append(m[j][i])
+        out.append(row)
+    return out
+
+def combine(row, col, meat, i, j):
+    out = []
+    for y in range(len(meat)):
+        newRow = []
+        for x in range(len(meat[0])):
+            if x == i:
+                newRow.append(col[y])
+                newRow.append(meat[x][y])
+            else:
+                newRow.append(meat[x][y])
+
 def main():
     vectors = loadtxt("vectors.txt")
     if linearlyDependant(vectors):
         print("The vectors are linearly dependant")
         return
     e_p = Gram_Schmidt(vectors)
+    print(row_echelon(vectors))
     for e in e_p:
         print(round_vector(e,3))
 main()
